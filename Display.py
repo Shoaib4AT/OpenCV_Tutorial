4baee09fb8d0aa7191e60bcabf3a342460a2257b
Shoaib4AT <shoaibashk.2000@gmail.com>
2019-06-29 02:00:02 +0530

    Count Object

--- Display.py
+++ Display.py
@@ -3,7 +3,7 @@ import cv2
 
 #%%
 # The first argument is the image
-image = cv2.imread(r'C:\Users\Shoaib Shaikh\Documents\OpenCV2\untitled.png')
+image = cv2.imread(r"C:\Users\Shoaib Shaikh\Documents\OpenCV2\DD3Smoke.jpg")
 
 
 #%%
@@ -15,17 +15,28 @@ gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blurred_image = cv2.GaussianBlur(image, (7,7), 0)
 
 #%%
-cv2.imshow("Original Image", image)
+# Show both our images
+cv2.imshow("Original image", image)
+cv2.imshow("Blurred image", blurred_image)
 
 
 #%%
-canny = cv2.Canny(blurred_image, 10, 30)
-cv2.imshow("Canny with low thresholds", canny)
+# Run the Canny edge detector
+canny = cv2.Canny(blurred_image, 320, 20)
+cv2.imshow("Canny", canny)
 
 #%%
-canny2 = cv2.Canny(blurred_image, 50, 150)
-cv2.imshow("Canny with high thresholds", canny2)
+contours, hierarchy= cv2.findContours(canny, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+
+#%%
+print("Number of objects found = ", len(contours))
+
+
+#%%
+cv2.drawContours(image, contours, -1, (0,255,0), 2)
+cv2.imshow("objects Found", image)
 cv2.waitKey(0)
+ 
 
 #%%
